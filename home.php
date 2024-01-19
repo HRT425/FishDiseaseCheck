33d2a6c9b208a339d76a0c643e428028093fb505
MARUYAMA Tsubasa <kd1300554@st.kobedenshi.ac.jp>
2024-01-19 14:20:19 +0900

    [add] home

---
+++ home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <div class="main_content">
+        <div class="main_content_area">
+            <button id="area">+</button>
+            <p class="main_content_area_text">Tap</p>
+        </div>
+    </div>
+    <div class="content">
+        <button id="content_btn">画像をアップロード</button>
+        <p class="content_text">ファイルをドロップ</p>
+    </div>
+</body>
+<footer>
+    <div class="footer_content">
+        <a class="footer_content_text1">記録</a>
+        <a class="footer_content_text2">ログアウト</a>
+    </div>
+</footer>
+</html>
