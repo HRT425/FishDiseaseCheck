5dc57e48b23ccc2e501d47a6255d90caea224922
MARUYAMA Tsubasa <kd1300554@st.kobedenshi.ac.jp>
2024-01-23 09:28:26 +0900

    [add] homeset

--- app/web/home.php
+++ app/web/home.php
@@ -6,21 +6,31 @@
     <link rel="stylesheet" href="./css/home.css">
 </head>
 <body>
+    <p class="daf">魚の健康状態</p>
     <div class="main_content">
         <div class="main_content_area">
-            <button id="area">+</button>
+            <button id="area">＋</button>
             <p class="main_content_area_text">Tap</p>
         </div>
     </div>
     <div class="content">
-        <button id="content_btn">画像をアップロード</button>
-        <p class="content_text">ファイルをドロップ</p>
+        <label class="upload-label">
+            <p class="daa">画像をアップロード</p>
+            <input type="file" id="example" multiple>
+        </label>
+        <!-- プレビュー画像を追加する -->
+        <div id="preview"></div>
     </div>
+    <br>
+    <script src="./js/home.js"></script>
 </body>
 <footer>
     <div class="footer_content">
-        <a class="footer_content_text1">記録</a>
-        <a class="footer_content_text2">ログアウト</a>
+        <hr>
+        <a class="footer_content_text1" type="button" onclick="location.href='./carender/carender.php'">　記　録　</a>
+        <a class="footer_content_text1">|</a>
+        <!--ログアウト.phpまだない-->
+        <a class="footer_content_text1" type="button" onclick="location.href='./carender/carender.php'">ログアウト</a>
     </div>
 </footer>
 </html>
---
+++ app/web/js/home.js
@@ -0,0 +1,29 @@
+function previewFile(file) {
+    // プレビュー画像を追加する要素
+    const preview = document.getElementById('preview');
+  
+    // FileReaderオブジェクトを作成
+    const reader = new FileReader();
+  
+    // ファイルが読み込まれたときに実行する
+    reader.onload = function (e) {
+      const imageUrl = e.target.result; // 画像のURLはevent.target.resultで呼び出せる
+      const img = document.createElement("img"); // img要素を作成
+      img.src = imageUrl; // 画像のURLをimg要素にセット
+      preview.appendChild(img); // #previewの中に追加
+    }
+  
+    // いざファイルを読み込む
+    reader.readAsDataURL(file);
+  }
+  
+  
+  // <input>でファイルが選択されたときの処理
+  const fileInput = document.getElementById('example');
+  const handleFileSelect = () => {
+    const files = fileInput.files;
+    for (let i = 0; i < files.length; i++) {
+      previewFile(files[i]);
+    }
+  }
+  fileInput.addEventListener('change', handleFileSelect);
